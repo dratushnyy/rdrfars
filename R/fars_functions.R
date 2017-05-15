62ea429b0d8420ccf58e008517fc4b551eb46f24
Dmitry Ratushyy <d.ratushnyy@gmail.com>
2017-05-15 08:46:08 +0300

    Added documentation for package

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -1,12 +1,12 @@
 #' Reads FARS (Fatality Analysis Reporting System) data from cvs file.
-#' 
+#'
 #' @import readr
 #' @import dplyr
-#' 
+#'
 #' @param filename A string.
 #' @section Warning:
 #' Function will stop if provided file not found
-#' 
+#'
 #' @return Loaded FARS data as \code{\link{dplyr::tbl_df}}.
 #' @examples
 #' fars_read("my_data.csv")
@@ -19,9 +19,9 @@ fars_read <- function(filename) {
         dplyr::tbl_df(data)
 }
 #' Genarates archive file name based on year value
-#' 
+#'
 #' @param year A number.
-#' 
+#'
 #' @return generated archive file name
 #' @examples
 #' make_filename("2011")
@@ -31,13 +31,13 @@ make_filename <- function(year) {
         sprintf("accident_%d.csv.bz2", year)
 }
 #' Read FARS (Fatality Analysis Reporting System) data for years
-#' 
+#'
 #' @import dplyr
-#' 
+#'
 #' @param years A vector of years values (numbers)
 #' @section Warning:
 #' Function will skip year if file with data for year does not exists
-#' 
+#'
 #' @return readed FARS data as \code{\link{dplyr::tbl_df}}.
 #' @examples
 #' fars_read_years(c(2001:2011))
@@ -47,7 +47,7 @@ fars_read_years <- function(years) {
                 file <- make_filename(year)
                 tryCatch({
                         dat <- fars_read(file)
-                        dplyr::mutate(dat, year = year) %>% 
+                        dplyr::mutate(dat, year = year) %>%
                                 dplyr::select(MONTH, year)
                 }, error = function(e) {
                         warning("invalid year: ", year)
@@ -56,37 +56,39 @@ fars_read_years <- function(years) {
         })
 }
 
-#' Summarize accidents by year and month
-#' 
+#' Summarize accidents from FARS data by year and month
+#'
 #' @import dplyr
 #' @param years A vector of years values (numbers) to summarize
 #' @section Warning:
 #' Function will skip year if file with data for year does not exists
-#' 
-#' @return summarized informaion about accidents 
+#'
+#' @return Summarized informaion about accidents
 #' @examples
 #' fars_summarize_years(c(2001:2011))
 #' fars_summarize_years(c("2004, 2008"))
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
-        dplyr::bind_rows(dat_list) %>% 
-                dplyr::group_by(year, MONTH) %>% 
+        dplyr::bind_rows(dat_list) %>%
+                dplyr::group_by(year, MONTH) %>%
                 dplyr::summarize(n = n()) %>%
                 tidyr::spread(year, n)
 }
-#' Summarize accidents by year and state 
-#' 
+
+#' Summarize accidents by year and state
+#'
 #' @import dplyr
 #' @import graphics
 #' @import maps?
-#' 
+#'
 #' @param state.num US state index (string)
 #' @param years A year value (number)
 #' @section Warning:
 #' Function will skip year if file with data for year does not exists
+#'
 #' Function will stop if invalid state provided
-#' 
-#' @return summarized informaion about accidents for year for state 
+#'
+#' @return summarized informaion about accidents for year for state
 #' @examples
 #' fars_summarize_years("01", 2001)
 fars_map_state <- function(state.num, year) {
