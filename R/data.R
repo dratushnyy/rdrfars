b3985d0fb64cff3074ac99ab02396dcbe29c1cc6
Dmitry Ratushyy <d.ratushnyy@gmail.com>
2017-05-16 21:36:29 +0300

    Added data documentation

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Fatality Analysis Reporting System for 2013 year
+#'
+#' @name accident_2013
+#' @docType data
+#' @references \url{https://www-fars.nhtsa.dot.gov/Main/index.aspx}
+#' @keywords data
+NULL
+
+#' Fatality Analysis Reporting System for 2014 year
+#'
+#' @name accident_2014
+#' @docType data
+#' @references \url{https://www-fars.nhtsa.dot.gov/Main/index.aspx}
+#' @keywords data
+NULL
+
+#' Fatality Analysis Reporting System for 2015 year
+#'
+#' @name accident_2015
+#' @docType data
+#' @references \url{https://www-fars.nhtsa.dot.gov/Main/index.aspx}
+#' @keywords data
+NULL
