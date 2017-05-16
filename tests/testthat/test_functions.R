eaea264ae2e035d278b30b3e1251fc41c79cd7ca
Dmitry Ratushyy <d.ratushnyy@gmail.com>
2017-05-16 09:35:53 +0300

    Add more tests

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -1,9 +1,27 @@
 test_that("can_read_existing_file", {
-  loaded <- fars_read("test_data_2013.csv")
+  loaded <- fars_read("accident_2013.csv")
   expect_is(loaded, "data.frame")
   expect_equal(length(loaded), 50)
 })
 
 test_that("stop_on_non_existing_file", {
   expect_error(fars_read("not_existing_file"))
+})
+
+test_that("warning_on_invalid_year_for_file_name", {
+  expect_warning(make_filename("abc"), 'NAs introduced by coercion')
+})
+
+test_that("reads_year_data", {
+  loaded <- fars_read_years(c(2013))
+})
+
+test_that("skip_read_data_on_invalid_year", {
+  expect_warning(fars_read_years(c("abc")), 'NAs introduced by coercion')
+  expect_warning(fars_read_years(c(2000)), 'invalid year: 2000')
+})
+
+
+test_that("stop_summary_on_invalid_state_id", {
+  expect_error(fars_map_state(-1, 2013))
 })
